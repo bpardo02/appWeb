cb06e6415733d24a7a18e9b85eb6641a781d3c8e
Benjamin <pardobenjamin.r@gmail.com>
2024-11-19 20:23:08 -0300

    personalizacion panel admin

--- gestionInmuebles/admin.py
+++ gestionInmuebles/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Inmueble
 
-admin.site.register(Inmueble)
+
+class InmuebleAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre", "direccion", "precio", "tipo", "fecha_creacion")
+
+    search_fields = ("nombre", "direccion", "tipo")
+
+    list_filter = ("tipo", "precio", "fecha_creacion")
+
+
+admin.site.register(Inmueble, InmuebleAdmin)
