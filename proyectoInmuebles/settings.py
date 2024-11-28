8d93db090a9bf2bb8713fccb1c09c73061755bef
Benjamin <pardobenjamin.r@gmail.com>
2024-11-28 20:00:44 -0300

    cambios interface e index

--- proyectoInmuebles/settings.py
+++ proyectoInmuebles/settings.py
@@ -123,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Ruta a tu carpeta de archivos estáticos
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
